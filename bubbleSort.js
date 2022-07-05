3a7609e99852f3f00f7c108c18291d5bb515d9c2
Promethesus <jeremyandrews707@gmail.com>
2022-07-05 11:37:02 -0700

    first commit

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+function sort(nums) {
+    let placeHolder = 0
+    let didSwapHappen = false
+    let iterations = 0
+    do{
+      didSwapHappen =false
+      for(i=0; i<nums.length-iterations; i++){
+        if(nums[i] > nums[i+1]){
+          placeHolder = nums[i];
+          nums[i] = nums[i+1];
+          nums[i+1] = placeHolder;
+          didSwapHappen = true
+        }
+      }
+  
+    }while(didSwapHappen)
+  }
