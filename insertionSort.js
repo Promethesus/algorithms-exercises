808ded61eb68fb0c8386c58cbf4fdc034322fe48
Promethesus <jeremyandrews707@gmail.com>
2022-07-06 08:44:44 -0700

    Add insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,16 @@
+let array = [2, 34, 3, 1, 5, 7, 8, 3, 5, 67, 534, 145, 234]
+
+
+function insertionSort(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let numberToInsert = nums[i];
+
+        for (let j = i - 1; nums[j] >= 0 && nums[j] > numberToInsert; j--) {
+            nums[j+1] = nums[j]
+            nums[j] = numberToInsert
+        }
+    }
+    return nums
+}
+
+console.log(insertionSort(array))
