61315247b9cdef167cba3242b5f9cc0f37adf88a
Promethesus <jeremyandrews707@gmail.com>
2022-07-06 09:54:58 -0700

    Recommit

--- insertionSort.js
+++ insertionSort.js
@@ -2,12 +2,12 @@ let array = [2, 34, 3, 1, 5, 7, 8, 3, 5, 67, 534, 145, 234]
 
 
 function insertionSort(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let numberToInsert = nums[i];
+    for (let current = 0; current < nums.length; current++) {
+        let numberToInsert = nums[current];
 
-        for (let j = i - 1; nums[j] >= 0 && nums[j] > numberToInsert; j--) {
-            nums[j + 1] = nums[j]
-            nums[j] = numberToInsert
+        for (let previous = current - 1; nums[previous] >= 0 && nums[previous] > numberToInsert; previous--) {
+            nums[previous + 1] = nums[previous]
+            nums[previous] = numberToInsert;
         }
     }
     return nums
